f579efe32b0b5d26161862accbc0316d3a436299
Uzo-Felix <uzochukwuonyekwelu03@gmail.com>
2023-09-12 12:44:51 +0100

    feat: connect mongodb and create new schema

--- index.js
+++ index.js
@@ -8,6 +8,30 @@ const app = express();
 const port = process.env.PORT || 3000;
 const mongoUri = process.env.MONGO_URI;
 
+// MongoDB connection
+mongoose.connect( mongoUri , {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+const db = mongoose.connection;
+db.on('error', console.error.bind(console, 'MongoDB connection error:'));
+db.once('open', () => {
+  console.log('Connected to MongoDB');
+});
+
+// Define a Person schema and model
+const personSchema = new mongoose.Schema({
+  name: String,
+});
+
+const Person = mongoose.model('Person', personSchema);
+
+// Middleware
+app.use(bodyParser.json());
+
+
+
 // Start the server
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
